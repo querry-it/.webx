b417db3d96989cfa271b4b5d26cbf2c34265701e
Nguyen Tuan Huy <githubhuy29082004@gmail.com>
2026-01-21 21:17:04 +0700

    09:16-21/01/2026 feature/login 111

--- frontend/src/components/SwitchLink.tsx
+++ frontend/src/components/SwitchLink.tsx
@@ -1,25 +1,3 @@
-// interface SwitchLinkProps {
-//     text: string;
-//     actionText: string;
-//     onClick?: (e: React.FormEvent<HTMLFormElement>) => void;
-// }
-
-// function SwitchLink({ text, actionText, onClick }: SwitchLinkProps) {
-//     return (
-//         <div className="flex justify-center items-center text-blue-600 text-[14px] mt-3 font-semibold">
-//             <p>
-//                 {text}{" "}
-//                 <button
-//                     type="button"
-//                     onClick={onClick}>
-//                     {actionText}
-//                 </button>
-//             </p >
-//         </div>
-//     );
-// }
-// export default SwitchLink;
-
 import { Link } from "react-router-dom";
 
 function SwitchLink({ text, actionText, to, onClick }: SwitchLinkProps) {
