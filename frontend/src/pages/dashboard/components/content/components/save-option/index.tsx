083f6d2235ddec57f4c7288f4842c8b89ad3d965
IchigoMazone <nhattrinh2702@gmail.com>
2026-02-02 18:09:30 +0700

    update save

--- frontend/src/pages/dashboard/components/content/components/save-option/index.tsx
+++ frontend/src/pages/dashboard/components/content/components/save-option/index.tsx
@@ -8,12 +8,15 @@ import {
     LockKeyhole,
     Logs,
     Luggage,
+    MapPin,
     Plus,
+    Search,
     Star,
+    X,
 } from "lucide-react";
 import classNames from "classnames/bind";
 import styles from "./save.module.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useMemo } from "react";
 import { useEditor } from "../../../../../../state/useEditor";
 
 const cx = classNames.bind(styles);
@@ -21,7 +24,7 @@ const cx = classNames.bind(styles);
 export default function SaveComponent() {
     const { state, dispatch } = useEditor();
     const Icon = useRef<{ x: number; y: number }>({ x: 20, y: 1.6 });
-    const [active, setActive] = useState<String>("list");
+    const [query, setQuery] = useState("");
 
     const setState = (option: string, key: string, value: boolean) => {
         dispatch({
@@ -173,8 +176,85 @@ export default function SaveComponent() {
     }
 
     function Add() {
+        const [inputValue, setInputValue] = useState<string>("");
+        const [activeSearch, setActiveSearch] = useState<boolean>(false);
+        const ref = useRef(null);
+        const MAX_ROWS = 4;
+
+        const autoResizeTextarea = (el: HTMLTextAreaElement | null) => {
+            if (!el) return;
+
+            el.style.height = "auto";
+
+            const style = window.getComputedStyle(el);
+            const lineHeight = parseFloat(style.lineHeight);
+            const padding =
+                parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+
+            const maxHeight = lineHeight * MAX_ROWS + padding;
+
+            if (el.scrollHeight > maxHeight) {
+                el.style.height = maxHeight + "px";
+                el.style.overflowY = "auto";
+            } else {
+                el.style.height = el.scrollHeight + "px";
+                el.style.overflowY = "hidden";
+            }
+        };
+
+        const suggestions = [
+            "Hồ Gươm, Hà Nội",
+            "Hồ Tây, Hà Nội",
+            "Phố cổ Hà Nội",
+            "Chợ Đồng Xuân",
+            "Nhà hát Lớn Hà Nội",
+            "Lăng Chủ tịch Hồ Chí Minh",
+            "Quảng trường Ba Đình",
+            "Văn Miếu Quốc Tử Giám",
+            "Hoàng Thành Thăng Long",
+            "Chùa Một Cột",
+            "Chùa Trấn Quốc",
+            "Nhà thờ Lớn Hà Nội",
+            "Cầu Long Biên",
+            "Phố đi bộ Hồ Gươm",
+            "Nhà tù Hỏa Lò",
+            "Bảo tàng Dân tộc học Việt Nam",
+            "Bảo tàng Lịch sử Quốc gia",
+            "Bảo tàng Mỹ thuật Việt Nam",
+            "Công viên Thống Nhất",
+            "Công viên Yên Sở",
+            "Hồ Thiền Quang",
+            "Hồ Trúc Bạch",
+            "Phủ Tây Hồ",
+            "Làng gốm Bát Tràng",
+            "Làng lụa Vạn Phúc",
+            "Thiên đường Bảo Sơn",
+            "Keangnam Landmark 72 Skywalk",
+            "Tháp nước Hàng Đậu",
+            "Ga Hà Nội",
+            "Phố sách 19/12",
+            "Phố bia Tạ Hiện",
+            "Chợ hoa Quảng Bá",
+            "Thung lũng hoa Hồ Tây",
+            "Công viên nước Hồ Tây",
+            "VinKE & Vinpearl Aquarium Times City",
+            "Aeon Mall Long Biên",
+            "Cầu Nhật Tân",
+            "Bãi đá sông Hồng",
+            "Đền Ngọc Sơn",
+            "Nhà hát Múa rối nước Thăng Long",
+        ];
+
+        const filteredSuggestions = useMemo(() => {
+            return suggestions.filter((item) =>
+                item.toLowerCase().includes(inputValue.toLowerCase()),
+            );
+        }, [inputValue]);
+
+        const showSuggestions = inputValue.trim().length > 0;
+
         return (
-            <div className={cx("add__bgr")}>
+            <div className={cx("add__bgr", { delete: activeSearch })}>
                 <div className={cx("space__add")}></div>
                 <div className={cx("name__add")}>
                     <div className={cx("name__content")}>
@@ -204,13 +284,21 @@ export default function SaveComponent() {
                     >{`Riêng tư - 0 địa điểm`}</span>
                 </div>
                 <div className={cx("note__add")}>
-                    <input
-                        type="text"
+                    <textarea
+                        ref={ref}
+                        rows={1}
                         placeholder="Thêm nội dung mô tả danh sách"
+                        onInput={(e) => autoResizeTextarea(e.currentTarget)}
                     />
                 </div>
                 <div className={cx("location__add")}>
-                    <div className={cx("add_item")}>
+                    <div
+                        className={cx("add_item")}
+                        onClick={() => {
+                            setActiveSearch(!activeSearch);
+                            setInputValue("");
+                        }}
+                    >
                         <span>
                             <Plus
                                 size={Icon.current.x}
@@ -220,8 +308,166 @@ export default function SaveComponent() {
                         <span>Thêm địa điểm</span>
                     </div>
                 </div>
-                <div className={cx("input__add")}></div>
-                <div className={cx("content__add")}></div>
+                <div className={cx("input__add")}>
+                    <div className={cx("input__content")}>
+                        <div className={cx("icon__left")}>
+                            <Search
+                                size={Icon.current.x}
+                                strokeWidth={Icon.current.y}
+                            />
+                        </div>
+                        <div className={cx("content")}>
+                            <input
+                                type="text"
+                                placeholder="Tìm kiếm địa điểm cần thêm vào"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                            />
+                        </div>
+                        {inputValue && (
+                            <div
+                                className={cx("icon__right")}
+                                onClick={() => setInputValue("")}
+                            >
+                                <X
+                                    size={Icon.current.x}
+                                    strokeWidth={Icon.current.y}
+                                />
+                            </div>
+                        )}
+                    </div>
+                    {showSuggestions && (
+                        <div className={cx("suggestions")}>
+                            {filteredSuggestions.length > 0 ? (
+                                filteredSuggestions.map((item, index) => (
+                                    <div
+                                        key={`${item}-${index}`}
+                                        className={cx("items")}
+                                        onMouseDown={(e) => {
+                                            e.preventDefault();
+                                            setInputValue(item);
+                                        }}
+                                    >
+                                        <MapPin
+                                            className={cx("icon")}
+                                            size={18}
+                                        />
+                                        <p className={cx("title")}>{item}</p>
+                                    </div>
+                                ))
+                            ) : (
+                                <div className={cx("items_x")}>
+                                    <p className={cx("title")}>
+                                        Không tìm thấy kết quả
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    <div
+                        className={cx("input__btn")}
+                        onClick={() => {
+                            setActiveSearch(true);
+                            setInputValue("");
+                        }}
+                    >
+                        <span>Xong</span>
+                    </div>
+                </div>
+                <div className={cx("content__add")}>
+                    <div className={cx("content__space")}></div>
+                    {Array.from({ length: 20 }).map((_, i) => (
+                        <LocationItem key={i} />
+                    ))}
+                    <div className={cx("content__space")}></div>
+                </div>
+            </div>
+        );
+    }
+
+    function LocationItem() {
+        const [hover, setHover] = useState<boolean>(false);
+        const [active, setActive] = useState<boolean>(false);
+        const refX = useRef(null);
+        const MAX_ROWS = 4;
+
+        const autoResizeTextarea = (el: HTMLTextAreaElement | null) => {
+            if (!el) return;
+
+            el.style.height = "auto";
+
+            const style = window.getComputedStyle(el);
+            const lineHeight = parseFloat(style.lineHeight);
+            const padding =
+                parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+
+            const maxHeight = lineHeight * MAX_ROWS + padding;
+
+            if (el.scrollHeight > maxHeight) {
+                el.style.height = maxHeight + "px";
+                el.style.overflowY = "auto";
+            } else {
+                el.style.height = el.scrollHeight + "px";
+                el.style.overflowY = "hidden";
+            }
+        };
+        return (
+            <div
+                className={cx("item__location", { noteX: active })}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            >
+                <div className={cx("location__img")}>
+                    <img
+                        src="https://lh3.googleusercontent.com/gps-cs-s/AHVAwer_J5-qDA7caFQ9qQJ5W19Ia1qKoSj3WwepiYI6n7ZECYEoYExWYTYC8SJPxL3b9LcPum4168sm2kP7PE5AEDfWsnH-tYqFxJwj2OFN6580QkWhd6oXbGrko67manH6VRUZfwvZ=w100-h115-p-k-no"
+                        alt="avatar"
+                    />
+                    {hover && (
+                        <span className={cx("delete")}>
+                            <X
+                                size={Icon.current.x}
+                                strokeWidth={Icon.current.y}
+                            />
+                        </span>
+                    )}
+                </div>
+                <div className={cx("location__content")}>
+                    <div className={cx("title")}>Lăng Chủ tịch Hồ Chí Minh</div>
+                    <div className={cx("star")}>
+                        4,7 <span className={cx("star__icon")}>★</span>
+                        (19.895)
+                    </div>
+                    <div className={cx("no")}></div>
+                    <div className={cx("type")}>Điểm mốc lịch sử</div>
+                    {!active && (
+                        <div
+                            className={cx("note")}
+                            onClick={() => {
+                                setActive(true);
+                            }}
+                        >
+                            <div className={cx("note__content")}>
+                                <span className={cx("icon")}>
+                                    <Plus
+                                        size={Icon.current.x}
+                                        strokeWidth={Icon.current.y}
+                                    />
+                                </span>
+                                <span className={cx("content")}>Ghi chú</span>
+                            </div>
+                        </div>
+                    )}
+                </div>
+                {active && (
+                    <div className={cx("location__note")}>
+                        <textarea
+                            ref={refX}
+                            rows={1}
+                            placeholder="Thêm ghi chú"
+                            onInput={(e) => autoResizeTextarea(e.currentTarget)}
+                        ></textarea>
+                    </div>
+                )}
             </div>
         );
     }
