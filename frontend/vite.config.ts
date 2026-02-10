9644fa9ee93f417db4220b768877028c7c5776ef
IchigoMazone <nhattrinh2702@gmail.com>
2026-02-10 11:17:52 +0700

    deploy via github actions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        host: true,
-        strictPort: false,
-        allowedHosts: [".trycloudflare.com"],
-    },
+  base: '/.webx/',
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    strictPort: false,
+    allowedHosts: ['.trycloudflare.com'],
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
