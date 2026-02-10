940b0434302211f5bed3955b59070362bca69068
IchigoMazone <nhattrinh2702@gmail.com>
2026-02-10 17:16:40 +0700

    update 6

--- frontend/src/pages/dashboard/components/content/components/introducerX-option/components/overview/index.tsx
+++ frontend/src/pages/dashboard/components/content/components/introducerX-option/components/overview/index.tsx
@@ -1,39 +1,53 @@
-import classNames from "classnames/bind";
-import styles from "./overview.module.css";
-import PlaceHero from "./components/PlaceHero/index.tsx";
-import PlaceAction from "./components/PlaceAction/index.tsx";
-import PlaceIntroducer from "./components/PlaceIntroducer/index.tsx";
-import PlaceInformation from "./components/PlaceInformation/index.tsx";
-import PlaceGallery from "./components/PlaceGallery/index.tsx";
-import ExteralArticleCard from "./components/ExternalArticleCard/index.tsx";
-
+import classNames from 'classnames/bind';
+import styles from './overview.module.css';
+import PlaceHero from './components/PlaceHero/index.tsx';
+import PlaceAction from './components/PlaceAction/index.tsx';
+import PlaceIntroducer from './components/PlaceIntroducer/index.tsx';
+import PlaceInformation from './components/PlaceInformation/index.tsx';
+import PlaceGallery from './components/PlaceGallery/index.tsx';
+import ExteralArticleCard from './components/ExternalArticleCard/index.tsx';
+import { useEditor } from '../../../../../../../../state/useEditor.ts';
+import { useEffect, useRef, useState } from 'react';
+import { X } from 'lucide-react';
 
 const cx = classNames.bind(styles);
 
 export default function Overview({ selectedPlace }) {
-
-    return (
-      <>
-        <div className={cx('sticky')}></div>
-        <div className={cx('location__content')}>
-          <PlaceHero place={selectedPlace} className={cx('place__hero')} />
-          <PlaceAction className={cx('place__action')} />
-          <PlaceIntroducer
-            place={selectedPlace}
-            className={cx('place__introducer')}
-          />
-          <PlaceInformation
-            place={selectedPlace}
-            className={cx('place__information')}
-          />
-          <PlaceGallery
-            place={selectedPlace}
-            className={cx('place__gallery')}
-          />
-          <ExteralArticleCard place={selectedPlace} className={cx('card')} />
+  const { state, dispatch } = useEditor();
+  const Icon = useRef<{ x: number; y: number }>({ x: 20, y: 1.6 });
+  const setState = (option: string, key: string, value: boolean) => {
+    dispatch({
+      type: option,
+      payload: { [key]: value },
+    });
+  };
+  return (
+    <>
+      <div
+        className={cx('sticky', {
+          'sticky--show': state.navbar_x.headerXy,
+        })}
+      >
+        <div className={cx('left')} />
+        <div className={cx('center')}>Văn Miếu - Quốc Tử Giám</div>
+        <div className={cx('right')}>
+          <X size={20} strokeWidth={2.2} />
         </div>
-      </>
-    );
+      </div>
+      <div className={cx('location__content')}>
+        <PlaceHero place={selectedPlace} className={cx('place__hero')} />
+        <PlaceAction className={cx('place__action')} />
+        <PlaceIntroducer
+          place={selectedPlace}
+          className={cx('place__introducer')}
+        />
+        <PlaceInformation
+          place={selectedPlace}
+          className={cx('place__information')}
+        />
+        <PlaceGallery place={selectedPlace} className={cx('place__gallery')} />
+        <ExteralArticleCard place={selectedPlace} className={cx('card')} />
+      </div>
+    </>
+  );
 }
-
-
--- frontend/src/state/editor.types.ts
+++ frontend/src/state/editor.types.ts
@@ -1,91 +1,92 @@
 export interface InformationState {
-    username: string;
+  username: string;
 }
 
 export interface SidebarState {
-    open: boolean;
+  open: boolean;
 }
 
 export interface NavbarXState {
-    introducerX: boolean;
-    Xreview: String;
-    save: String;
-    back: boolean;
-    dynamic: boolean;
-    activeX: { type: string | null };
-    active: {
-        type: string | null;
-        value: string | null | number;
-    };
-    hover: {
-        type: string | null;
-        value: string | null | number;
-    };
-    project: boolean;
-    group: boolean;
-    chat: boolean;
-    open_project: boolean;
-    open_group: boolean;
-    open_chat: boolean;
-    open_index_project: null | number;
-    index_project: null | boolean | number;
-    index_project_prev: null | boolean | number;
-    index_group: null | boolean | number;
-    index_group_prev: null | boolean | number;
-    index_chat: null | boolean | number;
-    list: {
-        type: string | null;
-        items: string[];
-    };
+  headerXy: boolean;
+  introducerX: boolean;
+  Xreview: String;
+  save: String;
+  back: boolean;
+  dynamic: boolean;
+  activeX: { type: string | null };
+  active: {
+    type: string | null;
+    value: string | null | number;
+  };
+  hover: {
+    type: string | null;
+    value: string | null | number;
+  };
+  project: boolean;
+  group: boolean;
+  chat: boolean;
+  open_project: boolean;
+  open_group: boolean;
+  open_chat: boolean;
+  open_index_project: null | number;
+  index_project: null | boolean | number;
+  index_project_prev: null | boolean | number;
+  index_group: null | boolean | number;
+  index_group_prev: null | boolean | number;
+  index_chat: null | boolean | number;
+  list: {
+    type: string | null;
+    items: string[];
+  };
 }
 
 export interface UtilState {
-    active: boolean;
-    loading: boolean;
-    hover: boolean;
-    error: string;
-    index: number | null;
-    open: boolean;
+  active: boolean;
+  loading: boolean;
+  hover: boolean;
+  error: string;
+  index: number | null;
+  open: boolean;
 }
 
 export interface DropdownState {
-    logout: boolean;
+  logout: boolean;
 }
 
 export interface TooltipState {
-    logo: boolean;
-    add: boolean;
-    search: boolean;
-    image: boolean;
-    spackle: boolean;
-    logout: boolean;
+  logo: boolean;
+  add: boolean;
+  search: boolean;
+  image: boolean;
+  spackle: boolean;
+  logout: boolean;
 }
 
 export interface ModalState {
-    profile: boolean;
-    update: boolean;
-    person: boolean;
-    setup: boolean;
-    help: boolean;
-    logout: boolean;
-    project: boolean;
+  profile: boolean;
+  update: boolean;
+  person: boolean;
+  setup: boolean;
+  help: boolean;
+  logout: boolean;
+  project: boolean;
 }
 
 export interface PositionState {
-    open_project: boolean;
-    project: {
-        top: number | null;
-        left: number | null;
-    };
+  open_project: boolean;
+  project: {
+    top: number | null;
+    left: number | null;
+  };
 }
 
 export interface EditorState {
-    information: InformationState;
-    sidebar: SidebarState;
-    navbar_x: NavbarXState;
-    util: UtilState;
-    dropdown: DropdownState;
-    tooltip: TooltipState;
-    modal: ModalState;
-    position: PositionState;
+  information: InformationState;
+  sidebar: SidebarState;
+  navbar_x: NavbarXState;
+  util: UtilState;
+  dropdown: DropdownState;
+  tooltip: TooltipState;
+  modal: ModalState;
+  position: PositionState;
 }
